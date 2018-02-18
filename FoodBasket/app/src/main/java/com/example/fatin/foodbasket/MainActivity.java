990355de5f18f5a94c96e16b31d5246472536b9b
Floyd <36140953+ziangzha@users.noreply.github.com>
2018-02-19 06:02:11 +0800

    Claim Page

--- FoodBasket/app/src/main/java/com/example/fatin/foodbasket/MainActivity.java
+++ FoodBasket/app/src/main/java/com/example/fatin/foodbasket/MainActivity.java
@@ -15,17 +15,38 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Find the button by its id from XML file(shareButton)
         Button button = findViewById(R.id.shareButton);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 Main2Activity();
+                //Log.d is to see if the button is actually working or not
                 Log.d("FoodBasket", "Share button pressed");
             }
         });
+
+        Button button1 = findViewById(R.id.claimButton);
+        button1.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view3){
+                Main4Activity(view3);
+
+            }
+        });
+
+
     }
     public void Main2Activity(){
+        //Open the user input page or second acitivity
         Intent intent = new Intent(this,Main2Activity.class);
+        startActivity(intent);
+
+    }
+
+    public void Main4Activity(View view3){
+        Intent intent3 = new Intent(this,Main4Activity.class);
+        startActivity(intent3);
 
     }
 }
