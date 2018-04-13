99cd00c730e883225ad5c5311c8055e8113ad73c
Fatin <wiieeiipk@gmail.com>
2018-04-13 13:10:07 -0400

    claim button with background

--- FoodBasket/app/build/generated/source/r/debug/android/support/v7/appcompat/R.java
+++ FoodBasket/app/build/generated/source/r/debug/android/support/v7/appcompat/R.java
@@ -694,7 +694,7 @@ public final class R {
         public static final int topPanel = 0x7f0900ca;
         public static final int uniform = 0x7f0900d2;
         public static final int up = 0x7f0900d3;
-        public static final int wrap_content = 0x7f0900db;
+        public static final int wrap_content = 0x7f0900dc;
     }
     public static final class integer {
         public static final int abc_config_activityDefaultDur = 0x7f0a0000;
@@ -731,16 +731,16 @@ public final class R {
         public static final int abc_search_view = 0x7f0b0018;
         public static final int abc_select_dialog_material = 0x7f0b0019;
         public static final int abc_tooltip = 0x7f0b001a;
-        public static final int notification_action = 0x7f0b002e;
-        public static final int notification_action_tombstone = 0x7f0b002f;
-        public static final int notification_template_custom_big = 0x7f0b0036;
-        public static final int notification_template_icon_group = 0x7f0b0037;
-        public static final int notification_template_part_chronometer = 0x7f0b003b;
-        public static final int notification_template_part_time = 0x7f0b003c;
-        public static final int select_dialog_item_material = 0x7f0b0040;
-        public static final int select_dialog_multichoice_material = 0x7f0b0041;
-        public static final int select_dialog_singlechoice_material = 0x7f0b0042;
-        public static final int support_simple_spinner_dropdown_item = 0x7f0b0044;
+        public static final int notification_action = 0x7f0b002f;
+        public static final int notification_action_tombstone = 0x7f0b0030;
+        public static final int notification_template_custom_big = 0x7f0b0037;
+        public static final int notification_template_icon_group = 0x7f0b0038;
+        public static final int notification_template_part_chronometer = 0x7f0b003c;
+        public static final int notification_template_part_time = 0x7f0b003d;
+        public static final int select_dialog_item_material = 0x7f0b0041;
+        public static final int select_dialog_multichoice_material = 0x7f0b0042;
+        public static final int select_dialog_singlechoice_material = 0x7f0b0043;
+        public static final int support_simple_spinner_dropdown_item = 0x7f0b0045;
     }
     public static final class string {
         public static final int abc_action_bar_home_description = 0x7f0e0001;
